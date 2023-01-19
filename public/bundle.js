e7c0f14ee97703fc79be3627222ca33daf897cc9
Kimsc9976 <tncks097@nvaer.com>
2023-01-20 00:18:59 +0900

    update : make connection with function

--- public/bundle.js
+++ public/bundle.js
@@ -21466,14 +21466,19 @@ const socket = io('/mediasoup')
 
 // TCP/IP 통신은 일반적으로 sokect통신 이라고 부른다. 
 
-socket.on('connection-success', ({socketId}) => {
+socket.on('connection-success', ({socketId, existProducer}) => {
   console.log("--", socketId);
 })
 
 let device;
 let rtpCapabilities;
 let producerTransport;
-let producer;
+let producer; //if some client is consumer -> we have to note that there is proudcer on server
+let consumerTransport;
+let consumer;
+
+let isProducer = false;
+
 let params = {
   //mediasoup params
   encoding : [
@@ -21499,15 +21504,17 @@ let params = {
   }
 }
 
-const streamSuccess = async (stream)=>{
+const streamSuccess = (stream)=>{
   localVideo.srcObject = stream;
   const track = stream.getVideoTracks()[0];
   params = {
     track, ...params
   }
+  goConnect(true) // for producer
 }
+
 const getLocalStream = () =>{
-  navigator.getUserMedia({
+  navigator.mediaDevices.getUserMedia({
     audio : false,
     video : {
       width : {
@@ -21521,16 +21528,38 @@ const getLocalStream = () =>{
     }
   }, streamSuccess, error=> {
     console.log(error.message);
+  }).then(streamSuccess)
+  .catch(error=>{
+    console.log(error.message)
   })
 }
 
+//we have to notify for when we are connecting as consumer or producer
+const goConnect = (producerORconsumer) =>{
+  isProducer = producerORconsumer;
+  device === undefined? getRtpCapabilities() : goCreateTransport() // do you have device ? No -> getRTPCapabilities // yes -> CreateTransport 
+}
+
+const goCreateTransport = () =>{
+  isProducer ? createSendTransport() : createRecvTransport()
+}
+
+const goConsume = () =>{
+  goConnect(false) // for consumer
+}
+
+
 const getRtpCapabilities = () =>{
-  socket.emit('getRtpCapabilities', (data) =>{
+  socket.emit('createRoom', (data) =>{
     console.log(`Router RTP Capabilities... ${data.rtpCapabilities}`)
-    console.log('data', data)
+    //we assign to local variable and will be used
+    // console.log('data', data)
     rtpCapabilities = data.rtpCapabilities
+    // once we have rtp capability, create device
+    createDevice()
   })
 }
+
 
 const createDevice = async () =>{
   try{
@@ -21538,7 +21567,10 @@ const createDevice = async () =>{
     await device.load({
       routerRtpCapabilities : rtpCapabilities
     })
-    console.log('RTP Capabilities', rtpCapabilities)
+
+    console.log('Device RTP Capabilities', rtpCapabilities)
+    //once the device load, create transport
+    goCreateTransport()
   }catch (error)
   {
     console.log(error)
@@ -21546,12 +21578,6 @@ const createDevice = async () =>{
       {console.warn('browser not supported')}
   }
 }
-
-
-btnLocalVideo.addEventListener('click', getLocalStream);
-btnRtpCapabilities.addEventListener('click', getRtpCapabilities)
-btnDevice.addEventListener('click',createDevice)
-
 
 // --> create device 이후에, 시행해야함
 //creating sendTransport
@@ -21596,10 +21622,10 @@ const createSendTransport = async ()=>{
         errback(error)
       }
     })
+
+    connectSendTransport()
   })
 }
-
-btnCreateSendTransport.addEventListener('click',createSendTransport)
 
   // for connect [Send transport & produce]
 const connectSendTransport = async()=>{
@@ -21608,14 +21634,76 @@ const connectSendTransport = async()=>{
   producer.on('trackened', ()=>{
     console.log('track ended')
     //close video tarck
+
   })
 
   producer.on('transportclose', ()=>{
     console.log('transport ended')
     //close video tarck
+
   })
 }
 
-btnConnectSendTransport.addEventListener('click',connectSendTransport)
 
+
+// create receiver transport
+const createRecvTransport = async ()=>{
+  socket.emit('createWebRTCTransport',{sender : false}, ({params})=>{
+    if (params.error){
+      console.log(params.error)
+      return
+    }
+
+    console.log(params)
+    consumerTransport = device.createRecvTransport(params)
+    consumerTransport.on('connect', async({dtlsParameters}, callback, error) =>{
+      try{
+        // signal of local DTLS parameters to the serverside transport
+        await socket.emit('transport-recv-connect',{
+          // transportId : consumerTransport.id,
+          dtlsParameters : dtlsParameters,
+        })
+
+        // tell the transport that parameters were transmitted
+        callback()
+      }catch(error){
+        errback(error)
+      }
+    })
+    connectRecvTransport()
+  })
+}
+
+
+const connectRecvTransport = async()=>{
+  await socket.emit('consume',{
+    rtpCapabilities : device.rtpCapabilities
+  },async({params}) =>{
+    if (params.error){
+      console.log('Cannot consume')
+      return
+    }
+    
+    console.log(params)
+
+    consumer = await consumerTransport.consume({
+      id : params.id,
+      producerId : params.producerId,
+      kind : params.kind,
+      rtpParameters : params.rtpParameters
+    })
+
+    const{track} = consumer
+
+    remoteVideo.srcObject = new MediaStream([track])
+
+    socket.emit('consumer-resume')
+  })
+}
+
+
+// for prodcer -> get capability + createdevice + create send transport + connect sendTranseport & produce
+// fur consumer -> get capability + createdevice + create receive transport + connect create receive transport
+btnLocalVideo.addEventListener('click', getLocalStream)
+btnRecvSendTransport.addEventListener('click', goConsume)
 },{"mediasoup-client":71,"socket.io-client":87}]},{},[99]);
